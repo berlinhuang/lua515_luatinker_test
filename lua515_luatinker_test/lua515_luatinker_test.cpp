952ed3301e8d46efb35c81dcafd8da0945d2b33c
berlinhuang <berlinmail@qq.com>
2018-01-10 18:40:21 +0800

    firstly commit
    firstly commit

---
+++ lua515_luatinker_test/lua515_luatinker_test.cpp
@@ -0,0 +1,16 @@
+// lua515_luatinker_test.cpp : 定义控制台应用程序的入口点。
+//
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+#include "sample1.h"
+#include "sample2.h"
+#include "sample3.h"
+#include "sample4.h"
+#include "sample5.h"
+int _tmain(int argc, _TCHAR* argv[])
+{
+	sample5();
+	return 0;
+}
+
